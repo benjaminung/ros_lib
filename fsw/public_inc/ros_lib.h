69569bf432704d2fa1c4426432b7f9e34872b549
Ben Ung <benjamin.ung@gmail.com>
2021-10-07 15:43:56 -0400

    ros_lib can subscribe to ros vector3 msgs

--- fsw/public_inc/ros_lib.h
+++ fsw/public_inc/ros_lib.h
@@ -9,21 +9,40 @@
 
 extern "C"
 {
-struct RosPub
+struct RosNode
 {
     ros::NodeHandle nodeHandle;
     ros::Publisher publisher;
+    ros::Subscriber subscriber;
 };
+
 #else
-struct RosPub;
-typedef struct RosPub RosPub;
+struct RosNode;
+typedef struct RosNode RosNode;
 #endif
 
+typedef enum RosNodeType {ROS_LIB_Publish, ROS_LIB_Subscribe} RosNodeType;
+typedef enum RosMessageType {ROS_LIB_Vector3, ROS_LIB_Twist} RosMessageType;
+
+typedef struct 
+{
+    double x;
+    double y;
+    double z;
+} cfsVector3;
+
 int32_t ROS_LIB_Init(void);
-RosPub * ROS_LIB_InitRos(int argc, char **argv);
-int8_t ROS_LIB_SendVectorToRos(RosPub *rosPub, double x, double y, double z);
+RosNode * ROS_LIB_InitRos(int argc, char **argv);
+bool ROS_LIB_InitPublisher(RosNode *rosNode, RosMessageType rosMsgType, char rosTopic[]);
+bool ROS_LIB_InitSubscriber(RosNode *rosNode, RosMessageType rosMsgType, char rosTopic[]);
+
+int8_t ROS_LIB_SendVectorToRos(RosNode *rosNode, double x, double y, double z);
+int ROS_LIB_GetVectorListSize();
+cfsVector3 ROS_LIB_GetNextVector();
+
+void ROS_LIB_SpinOnce();
 int32_t ROS_LIB_Test(void);
-void ROS_LIB_DeleteRosPub(RosPub *rosPub);
+void ROS_LIB_DeleteRosNode(RosNode *rosNode);
 
 #ifdef __cplusplus
 }
--- fsw/src/ros_lib.cpp
+++ fsw/src/ros_lib.cpp
@@ -1,33 +1,94 @@
 #include "ros_lib_version.h"
 #include "ros_lib_internal.hpp"
+#include <deque>
+
+std::deque<cfsVector3> vectorList;
 
 int32_t ROS_LIB_Init()
 {
     return 0;
 }
 
-RosPub * ROS_LIB_InitRos(int argc, char **argv)
-{
-    ros::init(argc,argv,"ros_lib");
-    RosPub * rosPub = new RosPub();
-    rosPub->publisher = rosPub->nodeHandle.advertise<geometry_msgs::Vector3>("ros_cfe_vectormsg", 100);
-    return rosPub;
+void msgCallback(const geometry_msgs::Vector3::ConstPtr& msg) 
+{ 
+  ROS_INFO("ROS_LIB: received data from ROS: %f,%f,%f", msg->x, msg->y, msg->z);
+  cfsVector3 rosVector;
+  rosVector.x = msg->x;
+  rosVector.y = msg->y;
+  rosVector.z = msg->z;
+  vectorList.push_back(rosVector);
 }
 
-int8_t ROS_LIB_SendVectorToRos(RosPub *rosPub, double x, double y, double z)
+RosNode * ROS_LIB_InitRos(int argc, char **argv)
+{
+    ros::init(argc,argv,"ros_lib");
+    RosNode *rosNode = new RosNode();
+    return rosNode;
+}
+
+bool ROS_LIB_InitPublisher(RosNode *rosNode, RosMessageType rosMsgType, char rosTopic[])
+{
+    switch (rosMsgType)
+    {
+        case ROS_LIB_Vector3:
+        rosNode->publisher = rosNode->nodeHandle.advertise<geometry_msgs::Vector3>(rosTopic, 100);
+        ROS_INFO("ROS_LIB: Setup to publish Vector3 msgs to %s topic complete.", rosTopic);
+        break;
+
+        default:
+        return false;
+    }
+    return true;
+}
+
+bool ROS_LIB_InitSubscriber(RosNode *rosNode, RosMessageType rosMsgType, char rosTopic[])
+{
+    switch (rosMsgType)
+    {
+        case ROS_LIB_Vector3:
+        rosNode->subscriber = rosNode->nodeHandle.subscribe<geometry_msgs::Vector3>(rosTopic, 100, msgCallback);
+        ROS_INFO("ROS_LIB: Setup to subscribe to Vector3 msgs from %s topic complete.",rosTopic);
+        break;
+
+        default:
+        return false;
+    }
+    return true;
+}
+
+int8_t ROS_LIB_SendVectorToRos(RosNode *rosNode, double x, double y, double z)
 {
     geometry_msgs::Vector3 msg;
     msg.x = x;
     msg.y = y;
     msg.z = z;
-    ROS_INFO("send ROS Vector3 msg: (%f,%f,%f), rostimenow.toSec: %f", msg.x, msg.y, msg.z , ros::Time::now().toSec());
-    rosPub->publisher.publish(msg);
+    ROS_INFO("ROS_LIB: send Vector3 msg to ROS: (%f,%f,%f), rostimenow.toSec: %f", msg.x, msg.y, msg.z , ros::Time::now().toSec());
+    rosNode->publisher.publish(msg);
     return 0;
 }
 
-void ROS_LIB_DeleteRosPub(RosPub *rosPub)
+int ROS_LIB_GetVectorListSize()
 {
-    delete rosPub;
+    return vectorList.size();
+}
+
+cfsVector3 ROS_LIB_GetNextVector()
+{
+    auto returnVec = vectorList[0];
+    vectorList.pop_front();
+    return returnVec;
+}
+
+void ROS_LIB_SpinOnce()
+{   
+    // ROS_INFO("ROS_LIB: SpinOnce");
+    ros::spinOnce();
+}
+
+void ROS_LIB_DeleteRosNode(RosNode *rosNode)
+{
+    ros::shutdown();
+    delete rosNode;
 }
 
 int32_t ROS_LIB_Test()
